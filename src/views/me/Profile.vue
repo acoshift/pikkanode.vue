5c213e341c404b701d8c6d0364397ae23d33b194
BioMaRu <Bio.Color.Palette@gmail.com>
2019-04-21 00:58:24 +0700

    profile: add blank state profile style

--- src/views/me/Profile.vue
+++ src/views/me/Profile.vue
@@ -6,7 +6,7 @@
 
             <div class="profile">
                 <img v-if="photo" class="avatar" :src="photo" height="200" @click="$refs.upload.click()">
-                <div v-else class="photo" @click="$refs.upload.click()">
+                <div v-else class="blank" @click="$refs.upload.click()">
                     <span>Click here to upload</span>
                 </div>
                 <input ref="upload" type="file" style="display: none" @change="upload">
@@ -137,11 +137,20 @@ export default {
         display: flex;
         align-items: center;
 
-        .avatar {
+        .avatar,
+        .blank {
             width: 100px;
             height: 100px;
             border-radius: 999px;
             cursor: pointer;
+        }
+
+        .blank {
+            display: flex;
+            align-items: center;
+            text-align: center;
+            padding: 0 0.5rem;
+            border: 3px dashed rgb(172, 172, 172);
         }
 
         h2 {
